499152bcdcbc8dd9d1d690cd865cdee6278c8402
Glitch (messenger-platform-quick-start) <none>
2023-04-19 14:53:06 +0000

    🌝🛬 Checkpoint ./nlp/index.js:56914353/2519

--- nlp/index.js
+++ nlp/index.js
@@ -1,53 +1,38 @@
 const { NlpManager } = require('node-nlp');
-const fs = require('fs');
-const path = require('path');
 const db = require('../models');
 
 const nlpManager = new NlpManager({ languages: ['es'] });
 
-// nlpManager.addDocument('es', d.input, d.output);
-// nlpManager.addAnswer('es', a.label, a.answer);
-
-const addDocumentsAndAnswers = (doc, ans) => {
-  doc.forEach(d => {
-    nlpManager.addDocument('es', d.input, d.output);
-  });
-
-  ans.forEach(a => {
-    nlpManager.addAnswer('es', a.label, a.answer);
-  });
+const loadDocumentsAndAnswers = async () => {
+  try {
+    const intents = await db.Intent.findAll();
+    for (const intent of intents) {
+      if (intent.tipo === 'DOCUMENT') {
+        nlpManager.addDocument(
+          'es',
+          intent.texto,
+          intent.intent
+        );
+      } else if (intent.tipo === 'ANSWER') {
+        nlpManager.addAnswer(
+          'es',
+          intent.intent,
+          intent.texto
+        );
+      }
+    }
+  } catch (error) {
+    console.error('Error al cargar los intents:', error);
+  }
 };
 
-const loadDocumentsAndAnswers = (folderPath) => {
-  const files = fs.readdirSync(folderPath);
+(async () => {
+  // Cargar los intents de la base de datos
+  await loadDocumentsAndAnswers();
 
-  files.forEach(file => {
-    const filePath = path.join(folderPath, file);
-    const { documents, answers } = require(filePath);
-    addDocumentsAndAnswers(documents, answers);
-  });
+  // Entrenar el modelo de NLP
+  nlpManager.train();
 
-};
-
-const loadEntitys = (folderPath) => {
-  const files = fs.readdirSync(folderPath);
-
-  files.forEach(file => {
-    const filePath = path.join(folderPath, file);
-    const { entity, titulo } = require(filePath);
-    nlpManager.addNamedEntityText(titulo, entity, 'es');
-  });
-
-};
-
-
-loadDocumentsAndAnswers(__dirname + '/intents');
-loadEntitys(__dirname + '/entitys');
-
-nlpManager.addDocument('es', 'omitir', 'omitir');
-nlpManager.addDocument('es', 'saltar', 'omitir');
-
-
-nlpManager.train();
-
-module.exports = nlpManager;
+  // Exportar el modelo de NLP
+  module.exports = nlpManager;
+})();
