8f2e8917237e63ab86fa3d6e6c5149a42487f9fd
Glitch (messenger-platform-quick-start) <none>
2023-04-26 20:50:12 +0000

    🎿👨 Checkpoint ./middlewares/clientTicketMiddleware.js:56914353/2220 ./services/index.js:56914353/372

---
+++ middlewares/clientTicketMiddleware.js
@@ -0,0 +1,50 @@
+const db = require('../models');
+const { WSProc, moment, regex, delay, TsToDateString } = require('../utils');
+const { ClienteService, TicketService, MessageService } = require('../services');
+const { whatsappMessage, messageInteractive, messageAction, messageObject, templateComponent } = require('../lib');
+
+const clientTicketMiddleware = async (req, res, next) => {
+  
+  const datos = WSProc(req.body);
+  
+  if("metadata" in datos){
+    req.app.Departamento = await db.Department.findOne({
+      where: {
+        phone_number: datos.metadata.display_phone_number, 
+        phone_number_id: datos.metadata.phone_number_id 
+      } 
+    });
+    if(req.app.Departamento === null){
+      return res.status(400).json({ error: 'Departamento no encontrado' });
+    }
+  }
+  if ("contacts" in datos){
+      //aqui se revisa y crea el cliente y el ticket, en caso de que el ticket se cree, se envia un mensaje notificando al cliente de que se a creado un ticket
+      req.app.waid = datos.contacts[0].wa_id;
+      req.app.Cliente = await ClienteService.crearClienteSiNoExiste(req.app.waid, datos.contacts[0].profile.name);
+      const TicketCheck = await TicketService.buscarOCrearTicket(req.app.waid, req.app.Departamento.id);
+      req.app.context = req.app.conversations.get(req.app.waid);
+      if (!context){
+        req.app.context = {};
+        req.app.conversations.set(req.app.waid, req.app.context);
+      }
+      req.app.Ticket = TicketCheck[0];
+      req.app.TicketData = await db.AdditionalInfo.findAll({
+        where: {
+          ticket_id: req.app.Ticket.id
+        }
+      });
+      
+      if(TicketCheck[1]){
+        req.app.context = {};
+        req.app.conversations.set(req.app.waid, req.app.context);
+        let msg = new whatsappMessage(req.app.Ticket.wa_id).createTextMessage(`Ticket creado exitosamente. ID asignado: ${String(req.app.Ticket.id).padStart(7, '0')}.`);      
+        MessageService.EnviarMensaje(req.app.Departamento, req.app.Ticket, msg)
+      }
+    }
+
+  // Pasa al siguiente middleware
+  next();
+};
+
+module.exports = clientTicketMiddleware;
---
+++ services/index.js
@@ -0,0 +1,10 @@
+const ClienteService = require('./clienteServices');
+const TicketService = require('./ticketServices');
+const MessageService = require('./messageServices');
+
+
+module.exports = {
+  ClienteService,
+  TicketService,
+  MessageService
+};
