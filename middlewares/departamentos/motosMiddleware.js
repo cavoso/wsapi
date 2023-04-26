aa5a46e3bcf14ceacb19be4c53c3c4f42def3cd8
Glitch (messenger-platform-quick-start) <none>
2023-04-26 21:47:16 +0000

    💐🚨 Checkpoint ./middlewares/departamentos/motosMiddleware.js:56914353/154

--- middlewares/departamentos/motosMiddleware.js
+++ middlewares/departamentos/motosMiddleware.js
@@ -1,6 +1,14 @@
-const db = require('./models');
-const validacion = require('./config/validaciones');
-const { WSProc, moment, regex, delay, TsToDateString } = require('./utils');
+const db = require('../../models');
+const validacion = require('../../config/validaciones');
+const { WSProc, moment, regex, delay, TsToDateString } = require('../../utils');
+
+const {
+  whatsappMessage,
+  messageInteractive,
+  messageAction,
+  messageObject,
+  templateComponent
+} = require('../../lib');
 
 
 const Middleware = (req, res, next) => {
