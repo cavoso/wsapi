324a4a877a2572211e4b59a3f1acf028525d87e3
Glitch (messenger-platform-quick-start) <none>
2023-04-26 19:05:11 +0000

    🚞👴 Checkpoint ./middlewares/statuses.js:56914353/395

--- middlewares/statuses.js
+++ middlewares/statuses.js
@@ -1,6 +1,18 @@
-const statusesMiddleware = (req, res, next) => {
+const db = require('./../models');
+const { WSProc, moment, regex, delay, TsToDateString } = require('./../utils');
+
+const statusesMiddleware = async (req, res, next) => {
   
-   const datos = WSProc(body);
+   const datos = WSProc(req.body);
+  
+  if("statuses" in datos){
+    //aqui se actualizan los estados de los mensajes
+    let mensaje = await db.Message.findOne({ where: { wamid: datos.statuses[0].id } });
+    if(mensaje){
+      mensaje.update({status: datos.statuses[0].status});
+    }
+    
+  }
 
   // Pasa al siguiente middleware
   next();
