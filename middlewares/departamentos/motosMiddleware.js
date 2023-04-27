b1bb89d8d4610cb36ea48846ed29ba8d35443ca2
Glitch (messenger-platform-quick-start) <none>
2023-04-27 18:14:59 +0000

    🏆🗻 Checkpoint ./middlewares/messageMiddleware.js:56914353/1016 ./middlewares/departamentos/motosMiddleware.js:56914353/893

--- middlewares/departamentos/motosMiddleware.js
+++ middlewares/departamentos/motosMiddleware.js
@@ -9,11 +9,22 @@ const Middleware = async (req, res, next) => {
   const datos = WSProc(req.body);
   const message = datos.messages[0]; 
   
+  for (const xentity of req.app.Departamento.entity) {
+    const encontrado = req.app.TicketData.find(item => item.key_name === xentity);
+    console.log(JSON.stringify(encontrado, null, 2));
+    /*const marcaEntity = req.app.response.entities.find(entity => entity.entity === xentity);
+    const valor = marcaEntity ? marcaEntity.option : null;
+    if(valor !== null){
+      req.app.TicketData = await TicketService.agregarInformacionExtra(req.app.Ticket.id,xentity, valor);
+    }
+    */
+  }
   
   switch(req.app.response.intent){
     case 'reserva':
       break;
     case 'verificacion':
+      //req.app.context.proceso
       break;
     case 'omitir':
       break;
