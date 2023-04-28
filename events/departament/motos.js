983e80dab34896d5de58b7c6421d7ed50bed5383
Glitch (messenger-platform-quick-start) <none>
2023-04-28 22:29:47 +0000

    🌍🎑 Checkpoint ./events/departament/motos.js:56914353/312

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -1,3 +1,8 @@
+const db = require('../models');
+const {  WSProc, moment, regex, delay, TsToDateString  } = require('../utils');
+const { ClienteService, TicketService, MessageService } = require('../services');
+const { whatsappMessage, messageInteractive, messageAction, messageObject, templateComponent } = require('../lib');
+
 module.exports = async function evento(response, eventData, conversations) {
 
   switch(response.intent){
