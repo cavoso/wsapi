87f3b0b340e904ce9558e36a045ad30346375c79
Glitch (messenger-platform-quick-start) <none>
2023-04-28 16:44:56 +0000

    🛥🐜 Checkpoint ./events/eventoContacts.js:56914353/188

--- events/eventoContacts.js
+++ events/eventoContacts.js
@@ -1,5 +1,8 @@
+const { ClienteService, TicketService, MessageService } = require('../services');
+
 module.exports = async function evento(eventData, data) {
   let wa_id = data.wa_id;
+  eventData.KeyContext = `${wa_id}_${eventData.Departamento.id}`
   console.log(JSON.stringify(data, null, 2));
   console.log(JSON.stringify(data.wa_id, null, 2));
 };
