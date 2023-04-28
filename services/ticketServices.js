1b048db9f69b482f08f9b1166575e5dad15390f7
Glitch (messenger-platform-quick-start) <none>
2023-04-28 18:08:46 +0000

    🐠🐙 Checkpoint ./services/ticketServices.js:56914353/237

--- services/ticketServices.js
+++ services/ticketServices.js
@@ -4,11 +4,12 @@ const { Op } = require('sequelize');
 async function buscarOCrearTicket(waid, deptoid) {
   try {
     // Buscar un ticket existente con el waid y estado no finalizado
-    const ticket = await db.Ticket.findOrCreate({
+    const result = await db.Ticket.findOrCreate({
       where: { wa_id: waid, department_id: deptoid, status: { [Op.ne]: 'CLOSED' } },
       defaults: { wa_id: waid, status: 'OPEN'}
     });
     // Devolver el objeto ticket (tanto si ya existía como si se acaba de crear)
+    const ticket = Array.isArray(result) ? result : [result, false];
     return ticket;
   } catch (error) {
     console.log(error);
