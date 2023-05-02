8459c007f3b2d228cf6eca04a967905c095bf78c
Glitch (messenger-platform-quick-start) <none>
2023-05-02 21:54:48 +0000

    🎠🛥 Checkpoint ./events/departament/motos.js:56914353/3574

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -5,19 +5,20 @@ const { whatsappMessage, messageInteractive, messageAction, messageObject, templ
 
 module.exports = async function evento(response, eventData, conversations) {
   eventData.updateRequisites();
-  console.log(JSON.stringify(eventData, null, 2));
+  //console.log(JSON.stringify(eventData, null, 2));
   switch(response.intent){
     case 'saludo':
-      if(eventData.context.saludobot){
-        //esto indica que es el primer saludo
+      let msg = "";
+      if(!eventData.context.saludobot){
+        msg = new whatsappMessage(eventData.Ticket.wa_id)
+        .createTextMessage(`¡Hola! Soy RSAsist, tu asistente virtual. Estoy aquí para ayudarte con tus consultas sobre la compra de motocicletas. Si necesitas ayuda o información sobre nuestros modelos, precios y disponibilidad, simplemente escribe "menu" en cualquier momento y te mostraré las opciones disponibles. ¡Estoy aquí para ayudarte en tu búsqueda de la motocicleta perfecta!`);
+        
+        eventData.context.saludobot = false;
       }else{
-        let msg = new whatsappMessage(eventData.Ticket.wa_id).createTextMessage(``);
-        await MessageService.EnviarMensaje(eventData.Departamento, eventData.Ticket, msg);
+        msg = new whatsappMessage(eventData.Ticket.wa_id)
+        .createTextMessage(`¡Hola de nuevo! Bienvenido de vuelta a RSAsist, tu asistente virtual. Si necesitas ayuda o tienes más preguntas sobre motocicletas, repuestos y más, no dudes en preguntar. Recuerda que puedes escribir "menu" en cualquier momento para ver las opciones disponibles. ¡Estoy aquí para ayudarte!`);
       }
-      /*
-      let msg = new whatsappMessage("").createTextMessage(`Ticket creado exitosamente. ID asignado: ${String(eventData.Ticket.id).padStart(5, '0')}.`);
       await MessageService.EnviarMensaje(eventData.Departamento, eventData.Ticket, msg);
-      */
       break;
     case 'verificacion':
       break;
@@ -28,5 +29,8 @@ module.exports = async function evento(response, eventData, conversations) {
     default:
       break;
   }
+  
+  eventData.updateRequisites();
+  conversations.set(eventData.Key_Context, eventData.context);
   
 };
