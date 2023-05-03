eac659f745a2c7bfa54b1813ae5ce059e4518076
Glitch (messenger-platform-quick-start) <none>
2023-05-03 14:01:55 +0000

    🎆🌊 Checkpoint ./events/departament/motos.js:56914353/100

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -19,6 +19,11 @@ module.exports = async function evento(response, eventData, conversations) {
         .createTextMessage(`¡Hola de nuevo! Soy RSAsist, tu asistente en la búsqueda de la moto ideal. Recuerda que puedes escribir "menu" en cualquier momento para ver opciones.`);
       }
       await MessageService.EnviarMensaje(eventData.Departamento, eventData.Ticket, msg);
+      
+      if(!eventData.context.departamentreq.marca){
+        
+      }
+      
       break;
     case 'verificacion':
       break;
