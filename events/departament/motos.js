f4682555d75ccab24e9b5a84ca88c71594826a59
Glitch (messenger-platform-quick-start) <none>
2023-05-04 14:35:26 +0000

    🎀🎤 Checkpoint ./events/departament/motos.js:56914353/2829

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -7,15 +7,46 @@ module.exports = async function evento(response, eventData, conversations, messa
   eventData.updateRequisites();
   //console.log(JSON.stringify(eventData, null, 2));
   console.log(JSON.stringify(response.intent, null, 2));
-  if(eventData.context.enproceso == ''){
-    
-  }else{
-    switch(eventData.context.enproceso){
-      case '':
-        break;
-    }
+  switch(response.intent){
+    case 'saludo':
+      let msg = "";
+      if(!eventData.context.saludobot){
+        msg = new whatsappMessage(eventData.Ticket.wa_id)
+          .createTextMessage(`¡Hola! Soy RSAsist, tu asistente en la búsqueda de la moto ideal. Escribe "menu" en cualquier momento para ver opciones.`);
+        eventData.context.saludobot = true;
+      }else{
+        msg = new whatsappMessage(eventData.Ticket.wa_id)
+          .createTextMessage(`¡Hola de nuevo! Soy RSAsist, tu asistente en la búsqueda de la moto ideal. Recuerda que puedes escribir "menu" en cualquier momento para ver opciones.`);
+      }
+      await MessageService.EnviarMensaje(eventData.Departamento, eventData.Ticket, msg);
+      delay(2000);
+      await GenerarMenu(eventData);
+      break;
+    case 'verificacion':
+      break;
+    case 'ciudad':
+      break;
+    case 'omitir':
+      break;
+    default:
+      if(eventData.context.enproceso === 'SelectMarca'){
+        let marcas = await db.MenuVehiculos.findAll({
+          where: {
+            padre: 0
+          }
+        });
+        console.log("entra aqui");
+      }else{
+        await MessageService.EnviarMensaje(
+          eventData.Departamento,
+          eventData.Ticket,
+          new whatsappMessage(eventData.Ticket.wa_id)
+          .createImageMessage("https://cdn.memegenerator.es/imagenes/memes/full/31/14/31142846.jpg")
+        );
+      }
+      
+      break;
   }
-  
   
   eventData.updateRequisites();
   conversations.set(eventData.Key_Context, eventData.context);
@@ -23,7 +54,7 @@ module.exports = async function evento(response, eventData, conversations, messa
 };
 
 async function GenerarMenu(eventData){
-  eventData.context.enproceso = "Menu";
+  
   let msgobject = new messageObject("Menu", "list");
   let msgmenu = "";
   let marcas = await db.MenuVehiculos.findAll({
@@ -31,7 +62,8 @@ async function GenerarMenu(eventData){
       padre: 0
     }
   });
-  if(!eventData.context.departamentreq.marca){    
+  if(!eventData.context.departamentreq.marca){
+    eventData.context.enproceso = "SelectMarca";
     msgmenu = "Por favor seleccione la marca";
     for(let marca of marcas){
       msgobject.addRow(marca.nombre, marca.id);
