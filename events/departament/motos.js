f25a9552fe46eebe3faea264c830261c1e0aa00d
Glitch (messenger-platform-quick-start) <none>
2023-05-05 13:44:25 +0000

    🐟💴 Checkpoint ./events/departament/motos.js:56914353/1685

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -8,54 +8,9 @@ const keyReply = "8fK2s";
 module.exports = async function evento(response, eventData, conversations, message) {
   eventData.updateRequisites();
   //console.log(JSON.stringify(eventData, null, 2));
-  console.log(eventData);
-  switch(response.intent){
-    case 'saludo':
-      let msg = "";
-      if(!eventData.context.saludobot){
-        msg = new whatsappMessage(eventData.Ticket.wa_id)
-          .createTextMessage(`¡Hola! Soy RSAsist, tu asistente en la búsqueda de la moto ideal. Escribe "menu" en cualquier momento para ver opciones.`);
-        eventData.context.saludobot = true;
-      }else{
-        msg = new whatsappMessage(eventData.Ticket.wa_id)
-          .createTextMessage(`¡Hola de nuevo! Soy RSAsist, tu asistente en la búsqueda de la moto ideal. Recuerda que puedes escribir "menu" en cualquier momento para ver opciones.`);
-      }
-      await MessageService.EnviarMensaje(eventData.Departamento, eventData.Ticket, msg);
-      delay(2000);
-      await GenerarMenu(eventData);
-      break;
-    case 'verificacion':
-      break;
-    case 'ciudad':
-      break;
-    case 'omitir':
-      break;
-    default:
-      if(response.utterance.includes(keyReply)){
-        let rev = response.utterance.split("_");
-        if(rev[1] === 'req'){
-          if(rev[2] === 'marca'){
-            let marca = await db.MenuVehiculos.findOne({
-              where: {
-                [rev[3]]: parseInt(rev[4], 10)
-              }
-            });
-            console.log(marca)
-          }
-          
-        }
-        
-        console.log("entra aqui");
-      }else{
-        await MessageService.EnviarMensaje(
-          eventData.Departamento,
-          eventData.Ticket,
-          new whatsappMessage(eventData.Ticket.wa_id)
-          .createImageMessage("https://cdn.memegenerator.es/imagenes/memes/full/31/14/31142846.jpg")
-        );
-      }
-      
-      break;
+  console.log(response);
+  if(response){
+    
   }
   
   eventData.updateRequisites();
