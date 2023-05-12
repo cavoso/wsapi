a2d81d34a1cd50815485fd40b3c017681b126694
Glitch (messenger-platform-quick-start) <none>
2023-05-12 15:03:47 +0000

    👌🐪 Checkpoint ./events/departament/motos.js:56914353/3102

--- events/departament/motos.js
+++ events/departament/motos.js
@@ -9,8 +9,7 @@ module.exports = async function evento(response, eventData, conversations, messa
   //console.log(JSON.stringify(eventData, null, 2));
   console.log(eventData);
   let detectedEntities = response.entities;
-  let intents = response.classifications;
-  console.log(intents);
+
   console.log(detectedEntities);
   
   if(response.answer){
@@ -22,101 +21,11 @@ module.exports = async function evento(response, eventData, conversations, messa
     );
   } 
   
-  if(eventData.context.departamentreq.marca === false){
-    //no esta seteada la marca
-    let detectedEntity = detectedEntities.find(entity => entity.entity === "marca");
-    if (detectedEntity){
-      //se reconocio la marca
-      eventData.TicketData = await TicketService.agregarInformacionExtra(eventData.Ticket.id, detectedEntity.entity, detectedEntity.option);
-      eventData.context.departamentreq.marca = true;
-      eventData.updateRequisites();
-      conversations.set(eventData.Key_Context, eventData.context);
-    }else{
-      //no se encontro la marca
-      let msgobject = new messageObject("Menu", "list");
-      let marcas = await db.MenuVehiculos.findAll({
-        where: {
-          padre: 0
-        }
-      });
-      for(let marca of marcas){
-        msgobject.addRow(marca.nombre, `${keyReply}_req_marca_${marca.id}`);
-      }
-      await MessageService.EnviarMensaje(
-        eventData.Departamento,
-        eventData.Ticket,
-        new whatsappMessage(eventData.Ticket.wa_id).createInteractiveMessage(
-          new messageInteractive("list").addBody("Por favor seleccione la marca").addFooter("RSAsist Menu").addAction(
-            new messageAction("list").addButton("Menu").addSection(msgobject.toJSON()).toJSON()
-          ).toJSON()
-        )
-      );
-      return ;
-    }
-  }else{
-    //ya esta seteada la marca
-    if(eventData.context.reply.includes(`${keyReply}_req_marca_`)){
-      eventData.context.reply = ""; 
-      await MessageService.EnviarMensaje(
-        eventData.Departamento, 
-        eventData.Ticket, 
-        new whatsappMessage(eventData.Ticket.wa_id)
-            .createTextMessage("Lo siento, pero la marca ya se encuentra registrada, si desea modificarla por favor utilice el menú")
-        );
-    }
-    
+  for(var requisito in eventData.context.requisitos){
+    console.log(requisito)
+    console.log(eventData.context.requisitos[requisito])
   }
-  
-  switch(response.intent){
-    case 'saludo':
-      break;
-    case 'reserva':
-      break;
-    case 'ciudad':
-      break;
-    case 'verificacion':
-      break;
-    case 'omitir':
-      break;
-    default:
-      break;
-  }
-  
-  
-  if(!eventData.context.requisitos.userdata){
-    if(!eventData.context.userdata.full_name){
-      
-    }
-  }
-  
-  
   
   
   
 };
-
-async function GenerarMenu(eventData){
-  
-  let msgobject = new messageObject("Menu", "list");
-  let msgmenu = "";
-  let marcas = await db.MenuVehiculos.findAll({
-    where: {
-      padre: 0
-    }
-  });
-  if(!eventData.context.departamentreq.marca){
-    msgmenu = "Por favor seleccione la marca";
-    for(let marca of marcas){
-      msgobject.addRow(marca.nombre, `${keyReply}_req_marca_id_${marca.id}`);
-    }
-  }
-  await MessageService.EnviarMensaje(
-    eventData.Departamento,
-    eventData.Ticket,
-    new whatsappMessage(eventData.Ticket.wa_id).createInteractiveMessage(
-      new messageInteractive("list").addBody(msgmenu).addFooter("RSAsist Menu").addAction(
-        new messageAction("list").addButton("Menu").addSection(msgobject.toJSON()).toJSON()
-      ).toJSON()
-    )
-  );
-}
